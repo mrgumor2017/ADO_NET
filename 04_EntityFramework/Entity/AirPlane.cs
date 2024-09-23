40048cf2cbab68175d345eb8822000e76e96a027
Bohdan <mrgumor2017@gmail.com>
2024-09-23 18:39:53 +0300

    )

---
+++ 04_EntityFramework/Entity/AirPlane.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace _04_EntityFramework.Entity
+{
+    public class AirPlane
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int MaxPassanger { get; set; }
+        public Collection<Flight> Flights { get; set; }
+    }
+}
---
+++ 04_EntityFramework/Entity/Client.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace _04_EntityFramework.Entity
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+        public Collection<Flight> Flights { get; set; }
+    }
+}
